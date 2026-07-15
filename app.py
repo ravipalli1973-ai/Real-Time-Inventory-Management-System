2ecb55772d116c41b3b914f88a0a447d1d4a783e
ravipalli1973-ai <ravipalli1973@gmail.com>
2026-07-15 16:44:11 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def delete(product_id):
     return redirect(url_for("products"))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
